f189bb3dc6b03824c78a27a0c9a745bd8a63e820
Cody Brunner <me@codybrunner.com>
2023-11-25 17:20:47 -0500

    feat(ex-09): adds algorithms project & linear search with tests

---
+++ algorithms/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+}
+
+func LinearSearch(array []int, target int) bool {
+	for _, v := range array {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
---
+++ algorithms/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestLinearSearch(t *testing.T) {
+	array := []int{1, 2, 3, 4, 5}
+	target := 3
+	want := true
+	got := LinearSearch(array, target)
+	if got != want {
+		t.Errorf("LinearSearch(%v, %v) = %v, want %v", array, target, got, want)
+	}
+}
