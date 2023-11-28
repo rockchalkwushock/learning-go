409f85570c214541941a6cea1baac7a3d420251b
Cody Brunner <me@codybrunner.com>
2023-11-27 21:00:40 -0500

    feat(ex-10): adds part 1 of quiz game

---
+++ quiz_game/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer'")
+	flag.Parse()
+
+	// Need to use *csvFilename because the "flag" package is returning a pointer to a string value.
+	file, err := os.Open(*csvFilename)
+
+	if err != nil {
+
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.answer {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d", correct, len(problems))
+}
+
+type Problem struct {
+	answer   string
+	question string
+}
+
+func parseLines(lines [][]string) []Problem {
+	ret := make([]Problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = Problem{
+			answer:   strings.TrimSpace(line[1]),
+			question: line[0],
+		}
+	}
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
